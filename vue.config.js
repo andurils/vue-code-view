9301ad3d7b148f123e3f60f7c765a909785342af
Anduril <egger@outlook.com>
2021-10-13 00:07:49 +0800

    🐳 chore: 新增依赖，配置项目构建

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,58 @@
 const path = require("path");
 const resolve = (dir) => path.join(__dirname, dir);
-// const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+
+// const CompressionWebpackPlugin = require("compression-webpack-plugin");
+// const zopfli = require("@gfx/zopfli");
+// const BrotliPlugin = require("brotli-webpack-plugin");
+
+// const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
 
 module.exports = {
   runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
   publicPath: process.env.VUE_APP_PUBLIC_PATH || "/",
-
+  // eslint-disable-next-line no-unused-vars
+  configureWebpack: (config) => {
+    // const plugins = [];
+    if (IS_PROD) {
+      config.optimization = {
+        splitChunks: {
+          cacheGroups: {
+            common: {
+              name: "chunk-common",
+              chunks: "initial",
+              minChunks: 2,
+              maxInitialRequests: 5,
+              minSize: 0,
+              priority: 1,
+              reuseExistingChunk: true,
+              enforce: true,
+            },
+            vendors: {
+              name: "chunk-vendors",
+              test: /[\\/]node_modules[\\/]/,
+              chunks: "initial",
+              priority: 2,
+              reuseExistingChunk: true,
+              enforce: true,
+            },
+            codemirrorJs: {
+              name: "chunk-codemirror",
+              test: /[\\/]node_modules[\\/]codemirror[\\/]/,
+              chunks: "all",
+              priority: 3,
+              reuseExistingChunk: true,
+              enforce: true,
+            },
+          },
+        },
+      };
+    }
+    // config.plugins = [...config.plugins, ...plugins];
+  },
   chainWebpack: (config) => {
     // 添加别名
     config.resolve.alias
@@ -34,5 +81,16 @@ module.exports = {
       .options({
         raw: true,
       });
+
+    if (IS_PROD) {
+      config.optimization.delete("splitChunks");
+
+      // 打包分析
+      config.plugin("webpack-report").use(BundleAnalyzerPlugin, [
+        {
+          analyzerMode: "static",
+        },
+      ]);
+    }
   },
 };
